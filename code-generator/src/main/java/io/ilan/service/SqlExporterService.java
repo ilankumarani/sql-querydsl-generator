bd21702f6456ac00346bcb9a21db935631593378
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-04-11 22:09:46 +0100

    Refactoring

--- code-generator/src/main/java/io/ilan/service/SqlExporterService.java
+++ code-generator/src/main/java/io/ilan/service/SqlExporterService.java
@@ -3,6 +3,8 @@ package io.ilan.service;
 import com.querydsl.sql.codegen.MetadataExporterConfigImpl;
 import io.ilan.config.MetadataExporterConfig;
 import io.ilan.util.CustomMetadataExporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.sql.SQLException;
 @Service
 public class SqlExporterService {
 
+    private static final Logger log = LoggerFactory.getLogger(SqlExporterService.class);
+
     @Autowired
     private MetadataExporterConfig metadataExporterConfig;
 
@@ -20,5 +24,6 @@ public class SqlExporterService {
         MetadataExporterConfigImpl var = metadataExporterConfig.getMetadataExporterConfig();
         CustomMetadataExporter exporter = new CustomMetadataExporter(var);
         exporter.export(connection.getMetaData());
+        log.info("########### GENERATION COMPLETED ###########");
     }
 }
