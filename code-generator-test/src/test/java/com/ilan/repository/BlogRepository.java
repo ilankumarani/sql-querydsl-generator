b43e912b56999255a1288dfca63a06ae529c3f38
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-04-13 11:28:51 +0100

    Refactoring of property key with Inclusive

--- code-generator-test/src/test/java/com/ilan/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ilan.repository;
-
-import com.ilan.entity.TestTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<TestTable, Long> {
-}
