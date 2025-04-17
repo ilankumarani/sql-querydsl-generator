533b1f7cfa737ed6dc014d03fc389ce381a4d7ed
ilankumaran <kumaranilan@yahoo.com>
2025-04-17 19:13:01 +0100

    Refactoring modules

---
+++ sample-example/entity-repo-test-jar/src/main/java/org/ilan/entity/Owner.java
@@ -0,0 +1,25 @@
+package org.ilan.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "OWNER_DETAILS", schema = "OWNER_SCHEMA")
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+}
