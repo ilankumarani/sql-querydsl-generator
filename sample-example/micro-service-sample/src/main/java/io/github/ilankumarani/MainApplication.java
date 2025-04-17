98b6be53aecdac376efe45b4503273223a181c32
ilankumaran <kumaranilan@yahoo.com>
2025-04-17 19:51:16 +0100

    Refactoring modules

--- sample-example/micro-service-sample/src/main/java/io/github/ilankumarani/MainApplication.java
+++ sample-example/micro-service-sample/src/main/java/io/github/ilankumarani/MainApplication.java
@@ -1,8 +1,12 @@
 package io.github.ilankumarani;
 
-@SpringBoo
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        SpringApplication.run(MainApplication.class, args);
     }
 }
--- tool-lib/code-generator/src/test/java/xio/ilan/test/GenerateTest.java
+++ tool-lib/code-generator/src/test/java/xio/ilan/test/GenerateTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import xio.ilan.Application;
 import xio.ilan.config.DbConfig;
 
