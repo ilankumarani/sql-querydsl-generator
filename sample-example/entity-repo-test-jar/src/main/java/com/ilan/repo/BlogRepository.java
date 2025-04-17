60dd62d4166f83d9da4794428da5dde855ee9e08
ilankumaran <kumaranilan@yahoo.com>
2025-04-17 20:19:30 +0100

    Refactoring modules

---
+++ sample-example/entity-repo-test-jar/src/main/java/com/ilan/repo/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.ilan.repo;
+
+import com.ilan.entity.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+}
---
+++ sample-example/entity-repo-test-jar/src/main/java/org/ilan/repo/OwnerRepository.java
@@ -0,0 +1,9 @@
+package org.ilan.repo;
+
+import org.ilan.entity.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
+}
